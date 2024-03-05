afb3e03cb7801a08b802bbc83e36d4766470006d
trilx <lextri980@gmail.com>
2024-03-05 11:09:49 +0700

    fix import

--- src/store/rootReducer.tsx
+++ src/store/rootReducer.tsx
@@ -1,5 +1,5 @@
 import { combineReducers } from "@reduxjs/toolkit";
-import exampleReducer from "./ExampleStore/ExampleReducer";
+import exampleReducer from "./exampleStore/ExampleReducer";
 
 export const rootReducer = combineReducers({
   example: exampleReducer,
