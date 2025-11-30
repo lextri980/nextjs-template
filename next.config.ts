4f54f3e0c1fb0ff27f163ed5d0cc8b31695c5c73
lextri980 <lextri980@gmail.com>
2025-11-30 16:05:30 +0700

    [base] update eslint

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  env: {
-    BASE_URL: process.env.BASE_URL,
-  },
-  eslint: {
-    // Treat warnings as errors during builds
-    ignoreDuringBuilds: false,
-  },
+  // env: {
+  //   BASE_URL: process.env.BASE_URL,
+  // },
+  // eslint: {
+  //   // Treat warnings as errors during builds
+  //   ignoreDuringBuilds: false,
+  // },
 };
 
 export default nextConfig;
