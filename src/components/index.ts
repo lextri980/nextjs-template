7ddfb31554ebeffd58832c9af8d9fdcb8ba6ac76
lextri980 <lextri980@gmail.com>
2026-02-02 22:09:08 +0700

    create tq base

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
-import Button from "./Button";
-import FormInput from "./FormInput";
-import Icon from "./Icon";
-
-export { Button, FormInput, Icon };
+export { default as Field } from './form-input/field';
+export { default as RadioGroup } from './form-input/radio-group';
+export { default as TextField } from './form-input/text-field';
+export { default as Label } from './label';
+export { default as Loading } from './loading';
+export { default as Toaster } from './toaster';
